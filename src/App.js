7fec0639188af90f69a392e491823c9c1b3a6dcb
Jadiê <jadieoliveira00@gmail.com>
2020-07-12 13:34:11 -0300

    reunindo tudo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { apiRequest } from "./store/actions";
 
 function App() {
-  return <div>Hello, world!</div>;
+  const { catImageUrl, quoteText } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const handleClick = () => dispatch(apiRequest());
+
+  useEffect(() => {
+    dispatch(apiRequest());
+  }, [dispatch]);
+
+  return (
+    <div className="container">
+      <h1>Cats + Qoutes</h1>
+      <div className="row">
+        <img src={catImageUrl} alt="A cat" />
+      </div>
+      <blockquote>{quoteText}</blockquote>
+      <button onClick={handleClick}>Gimme more...</button>
+    </div>
+  );
 }
 
 export default App;
